adf537314743e2eab018e3838369c99dfc93e780
Giuseppe Cristaudo <93075645+CristaudoGiuseppe@users.noreply.github.com>
2022-12-23 14:15:13 +0100

    Main commit

---
+++ Program.cs
@@ -0,0 +1,178 @@
+﻿using HtmlAgilityPack;
+using System.Net;
+using NLog;
+
+
+namespace MyAppFree
+{
+    class Program
+    {
+        // Declare variables
+        private static Logger logger = LogManager.GetCurrentClassLogger(); // used to log main operations and errors
+        private static string proxiesPath = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName.Replace(@"bin\", "proxies.txt");
+        private static string urlPlayStore = "https://play.google.com/store/apps"; 
+        
+
+        static async Task Main(string[] args)
+        {
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            try
+            {
+                // Declare variables
+                List<string> proxies = new List<string>();
+                Random rand = new Random();
+                string html;
+
+                // Logger configuration
+                configNlog();
+                logger.Debug("DEBUG PARTITO");
+                try
+                {
+                    proxies = ReadProxies(proxiesPath);
+                }
+                catch (Exception ex)
+                {
+                    logger.Error("Unable to read proxies: " + ex.Message);
+                }
+                if (proxies.Count == 0)
+                {
+                    Console.WriteLine("No proxies loaded..");
+                    logger.Info("No proxies loaded..");
+                    html = await GetHTMLContent(urlPlayStore, "");
+                }
+                else // make an HTTP request using proxies
+                {
+                    html = await GetHTMLContent(urlPlayStore, proxies[rand.Next(proxies.Count)]);
+                }
+
+                // Load the HTML into a HtmlDocument
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(html);
+
+                // Use the SelectNodes method and an XPath expression to find all the "a" elements with a "href" attribute
+                HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes("//a[@href]");
+
+                var hrefList = nodes.Where(node => node.GetAttributeValue("href", "").Contains("?id="))
+                                   .Select(node => node.GetAttributeValue("href", "")).ToList();
+
+
+
+                // Option 1: thread -> too complex
+                // Option 2: Parallel.ForEach -> best in theory for sharding but does not work
+                // Option 3: Task -> fast and easy to use
+                // Option 4: TDL Block -> way too slow
+
+                List<Task> tasks = new List<Task>();
+
+                foreach (string appid in hrefList)
+                {
+                    // create a new task to process the element
+                    Task task = Task.Run(async () =>
+                    {
+                        if (proxies.Count > 0)
+                        {
+                            ScrapeSingleApp ssa = new ScrapeSingleApp(appid, proxies[rand.Next(proxies.Count)]);
+                            await ssa.Start();
+                        }
+                        else
+                        {
+                            ScrapeSingleApp ssa = new ScrapeSingleApp(appid);
+                            await ssa.Start();
+                        } 
+                    });
+
+                    tasks.Add(task);
+                }
+
+                Task.WaitAll(tasks.ToArray());
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+
+            watch.Stop();
+            Console.WriteLine($"Elapsed time: {watch.ElapsedMilliseconds} ms");
+        }
+
+
+        public static async Task<string> GetHTMLContent(string URL, string proxy)
+        {  
+            // Create a web client to download the HTML from the URL
+            using (WebClient client = new())
+            {
+                if (proxy.Equals(""))
+                {
+                    return await client.DownloadStringTaskAsync(URL);
+                }
+                else
+                {
+                    client.Proxy = new WebProxy(proxy);
+                    return await client.DownloadStringTaskAsync(URL);
+                }
+            }
+        }
+
+
+        static List<string> ReadProxies(string filePath)
+        {
+            List<string> validProxies = new List<string>();
+
+            if (File.Exists(filePath))
+            {
+                string[] proxyList = File.ReadAllLines(filePath);
+
+                if (proxyList.Length > 0)
+                {
+                    foreach (string proxy in proxyList)
+                    {
+                        if (IsValidProxyFormat(proxy))
+                        {
+                            validProxies.Add(proxy);
+                        }
+                    }
+                }
+            }
+
+            return validProxies;
+        }
+
+        static bool IsValidProxyFormat(string proxy)
+        {
+            if (!proxy.StartsWith("http://") && !proxy.StartsWith("https://"))
+            {
+                return false;
+            }
+
+            if (!proxy.Contains(':') || !char.IsNumber(proxy, proxy.LastIndexOf(':') + 1))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        static void configNlog()
+        {
+            string fileLog = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName.Replace("bin", "log.txt");
+
+            var config = new NLog.Config.LoggingConfiguration();
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = fileLog };
+ 
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logfile);
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+
+            LogManager.Configuration = config;
+        }
+
+        static async Task TestFunc()
+        {
+            ScrapeSingleApp ssa1 = new("com.nianticlabs.pokemongo");
+            ScrapeSingleApp ssa2 = new("com.king.candycrushsaga");
+            await ssa1.Start();
+            await ssa2.Start();
+        }
+
+    }
+}
---
+++ ScrapeSingleApp.cs
@@ -0,0 +1,122 @@
+﻿using System.Net;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
+namespace MyAppFree
+{
+
+    class ScrapeSingleApp
+    {
+        private string FileName;
+        private string name = "";
+        private string category = "";
+        private string stars = "";
+        private string VersioneCorrente = "";
+        private string URL;
+        private string proxy;
+
+        public ScrapeSingleApp(string aFileName, string aProxy)
+        {
+            FileName = aFileName.Replace("/store/apps/details?id=", "");
+            proxy= aProxy;
+            URL = "https://play.google.com/store/apps/details?id=" + FileName;
+        }
+
+        public ScrapeSingleApp(string aFileName)
+        {
+            FileName = aFileName.Replace("/store/apps/details?id=", "");
+            proxy = "";
+            URL = "https://play.google.com/store/apps/details?id=" + FileName;
+        }
+
+        public async Task Start()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            Console.WriteLine("Starting: " + FileName);
+            await ScrapeHtml();
+            SaveJson();
+            watch.Stop();
+            //Console.WriteLine(watch.ElapsedMilliseconds);
+        }
+
+        private async Task ScrapeHtml()
+        {
+            try
+            {
+                string html = await Program.GetHTMLContent(URL, proxy);
+                // Load the HTML into a HtmlDocument
+                HtmlDocument doc = new();
+              
+
+                doc.LoadHtml(html);
+                
+                // Get App Name using XPath
+                name = doc.DocumentNode.SelectSingleNode("//h1").InnerText;
+                //Console.WriteLine(name);
+
+                // Get App Category using CPath
+                HtmlNodeCollection categoryNodes = doc.DocumentNode.SelectNodes("//span[@jsname='V67aGc' and @class='VfPpkd-vQzf8d']");
+                HtmlNode mainCategoryNode = categoryNodes[3];
+                category = mainCategoryNode.InnerText;
+                //Console.WriteLine(category);
+
+                // Get App Version using REGEX
+                string versionRegex = "You can request that data be deleted"; // it seems the safest option
+                MatchCollection versionMatches = Regex.Matches(html, versionRegex);
+                Match mainVersionMatch = versionMatches[1];
+
+                // Extract the phrase from the input string
+                string versionSubstring = html.Substring(mainVersionMatch.Index, 100);
+                VersioneCorrente = versionSubstring.Split("null,[[[")[1].Split("]]")[0].Replace("\"", "");
+                //Console.WriteLine(VersioneCorrente);
+
+                // Get App Starts using XPath
+                stars = doc.DocumentNode.SelectSingleNode("//div[@itemprop='starRating']").InnerText.Replace("star", "");
+                //Console.WriteLine(stars);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unable to get APP info");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void SaveJson()
+        {
+            //Console.WriteLine("Saving JSON file...");
+            // Create a dictionary and populate its keys and values
+            Dictionary<string, string> data = new();
+            data["Name"] = name;
+            data["Category"] = category;
+            data["Stars"] = stars;
+            data["Versione corrente"] = VersioneCorrente;
+
+            // Serialize the data to a JSON string
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+
+            Console.WriteLine(json);
+            try
+            {
+                string workingDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName.Replace("bin", @"result");
+                string path = Path.Combine(workingDirectory, FileName + ".json");
+                if (File.Exists(path))
+                {
+                    Console.WriteLine("File " + FileName + " already exists");
+                }
+                else
+                {
+                    File.WriteAllText(path, json);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Failed at saving " + FileName);
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        
+    }
+}
